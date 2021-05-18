a446468360277f18dbb0d0255a43b601b214372a
valpr <me@andrewfong.ca>
2021-05-17 21:54:01 -0400

    createMarker testing

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useActions } from '../hooks/useActions';
 //maybe this can slide in eventually
 const SideMenu: React.FC = () => {
     const { createMarker } = useActions();
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [lat, setLat] = useState<number>(0);
+    const [lng, setLng] = useState(0);
     const onClick = () => {
         //test out CreateMarker action here
         createMarker({
-            id: 0,
-            lat: -1.3884,
-            lng: 22,
-            description: 'wow!',
-            title: 'test',
+            id: Math.random(),
+            lat,
+            lng,
+            description,
+            title,
         });
     };
 
     return (
         <div className="SideMenu">
-            Title: <input></input>
+            Title:
+            <input value={title} onChange={(e) => setTitle(e.target.value)} />
             Description
-            <textarea></textarea>
+            <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
             lat:
-            <input />
+            <input
+                type="number"
+                value={lat}
+                onChange={(e) => setLat(parseInt(e.target.value))}
+            />
             lng:
-            <input />
+            <input
+                type="number"
+                value={lng}
+                onChange={(e) => setLng(parseInt(e.target.value))}
+            />
             <button onClick={onClick}>Submit</button>
         </div>
     );
